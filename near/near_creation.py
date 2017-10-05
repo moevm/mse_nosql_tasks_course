db4350bd3efa95977526df9011ac023b11b571b1
awethon <gorodgorod63@gmail.com>
2017-10-05 23:29:28 +0300

    near added

---
+++ near/near_creation.py
@@ -0,0 +1,13 @@
+import numpy as np
+from pymongo import MongoClient, GEOSPHERE
+
+mean = [55.75410148414108, 37.62049198150635]
+cov = [[0.04, 0],[0, 0.04]]
+xy = np.random.multivariate_normal(mean, cov, 100000)
+
+client = MongoClient()
+db = client.nr
+db.places.create_index([('loc', GEOSPHERE)])
+
+for item in xy:
+    db.places.insert_one({'loc': {'type' : 'Point','coordinates' : item.tolist() }})
