085242a02f9abb01bdf1f45f611ee597d1ae9d9a
MilanaGaiduk <milagaiduk@gmail.com>
2017-10-10 22:40:48 +0300

    generates a DB, solves a task and checks student's answer

---
+++ geointersects_task/test.py
@@ -0,0 +1,12 @@
+def test_connection(s):
+    assert s.run('true').succeeded, "Could not connect to server"
+
+def test_file_exists(s):
+    cmd = 'test -f /home/box/answer'
+    assert s.run(cmd).succeeded, "File not found"
+
+def test_file_content(s):
+    correct_answer = '[135.26, 67.07], [132.45, 61.02], [138.25, 62.63], [145.11, 67.20], [135.26, 67.07]'
+    user_answer = s.run('cat /home/box/answer')
+
+    assert user_answer == correct_answer, "Ваш ответ не совпадает с правильным."
