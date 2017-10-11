24dbcc519ce4f78370e62a66ba1dc63bfb7276bd
awethon <gorodgorod63@gmail.com>
2017-10-11 10:28:03 +0300

    answer simplified

--- geowithin_task1/geowithin_solution.py
+++ geowithin_task1/geowithin_solution.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient, GEOSPHERE
 import json
-from statistics import mean
 
 client = MongoClient()
 db = client.sw_db
@@ -12,8 +11,4 @@ result = list(db.places.find({'geometry':
                             }
                      ))
 
-
-x = mean([mean([item.get('geometry').get('coordinates')[0][0], item.get('geometry').get('coordinates')[1][0]]) for item in result])
-y = mean([mean([item.get('geometry').get('coordinates')[0][1], item.get('geometry').get('coordinates')[1][1]]) for item in result])
-
-open('correct_answer', 'w').write(' '.join(list(map(lambda x: str(round(x, 6)), [x, y, len(result)/len(list(db.places.find()))]))))
+open('correct_answer', 'w').write(str(len(result)/len(list(db.places.find()))))
