4bc71f3763955d6b0f35eafc2b867097a009bc55
awethon <gorodgorod63@gmail.com>
2017-10-11 10:41:59 +0300

    pep8

--- geowithin_task1/geowithin_test.py
+++ geowithin_task1/geowithin_test.py
@@ -2,6 +2,7 @@ def test_file_exists(s):
     cmd = 'test -f /home/box/answer'
     assert s.run(cmd).succeeded, "File not found"
 
+
 def test_file_content(s):
     correct_answer = '-73.966509 40.78132 0.349515'
     user_answer = s.run('cat /home/box/answer')
