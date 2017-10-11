d6c284f4b7d6e42eda73c5096319778e2368c8e0
awethon <gorodgorod63@gmail.com>
2017-10-11 11:21:28 +0300

    im tired of writing commits

--- geowithin_task1/geowithin_solution.py
+++ geowithin_task1/geowithin_solution.py
@@ -7,11 +7,10 @@ client = MongoClient()
 db = client.sw_db
 
 result = db.places.find({'geometry':
-                                {'$geoWithin':
-                                    {'$geometry' : json.loads(open("polygon.json", "r").read())}
-                                }
+                            {'$geoWithin':
+                                {'$geometry' : json.loads(open("polygon.json", "r").read())}
                             }
-                     ).count()
+                        }).count()
 
 open('correct_answer', 'w').write(str(result))
 client.drop_database('sw_db')
