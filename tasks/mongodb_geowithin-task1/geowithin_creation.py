9c7f394d8cb8b71898e0f57b8526179a64019fec
awethon <gorodgorod63@gmail.com>
2017-10-24 13:54:59 +0300

    fixed bug

--- tasks/mongodb_geowithin-task1/geowithin_creation.py
+++ tasks/mongodb_geowithin-task1/geowithin_creation.py
@@ -9,6 +9,5 @@ client = MongoClient()
 db = client.sw_db
 db.places.create_index([('geometry', GEOSPHERE)])
 lasers = json.load(open('lasers.json', 'r'))
-open('laser1.json', 'w').write(str([{"geometry" : laser.get("geometry")} for laser in lasers]).replace("'", '''"'''))
 indicies = random.sample(range(len(lasers)), ceil(len(lasers)*0.75))
 db.places.insert_many([lasers[ind] for ind in indicies])
