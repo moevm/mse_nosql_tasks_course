d4dfebb10217d2efb27dba874694c1af863aab9f
awethon <gorodgorod63@gmail.com>
2017-10-24 11:57:48 +0300

    bugfix

--- tasks/mongodb_geowithin-task1/generate_conditions.sh
+++ tasks/mongodb_geowithin-task1/generate_conditions.sh
@@ -6,10 +6,14 @@ key="esawwr3Xv41aSZCcVgz5AxcsBBBvtREssZEbFfbz3SCqzXBDSg"
 #mongo ${db_name} --eval 'db.test.insert({"name":"foo"});' --quiet
 #echo "${db_name}" > /home/box/correct_answer
 
-cd /home/box/
+cd /home/box/repo/tasks/mongodb_geowithin-task1/
 
 python3 geowithin_creation.py
 python3 geowithin_solution.py
+
+mv correct_answer /home/box/
+
+cd /home/box/
 
 echo ${key} | gpg --no-tty --passphrase-fd 0 -c /home/box/correct_answer
 chown box:box /home/box/correct_answer.gpg
