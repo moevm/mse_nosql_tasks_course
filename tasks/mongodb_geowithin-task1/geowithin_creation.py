8fd047b95e7d1edd99b1a6d927bec4e1291402aa
awethon <gorodgorod63@gmail.com>
2017-10-24 14:49:02 +0300

    back to geometry

--- tasks/mongodb_geowithin-task1/geowithin_creation.py
+++ tasks/mongodb_geowithin-task1/geowithin_creation.py
@@ -7,7 +7,7 @@ from math import ceil
 
 client = MongoClient()
 db = client.sw_db
-db.places.create_index([('loc', GEOSPHERE)])
+db.places.create_index([('geometry', GEOSPHERE)])
 lasers = json.load(open('lasers.json', 'r'))
 indicies = random.sample(range(len(lasers)), ceil(len(lasers)*0.75))
 db.places.insert_many([lasers[ind] for ind in indicies])
--- tasks/mongodb_geowithin-task1/geowithin_solution.py
+++ tasks/mongodb_geowithin-task1/geowithin_solution.py
@@ -5,17 +5,16 @@ import json
 
 client = MongoClient()
 db = client.sw_db
-
-result = db.places.find({'loc':
+result = db.places.find({'geometry':
                             {'$geoWithin':
-                                {'$geometry' :
+                                {'$geometry':
                                     {
                                         "type": "Polygon",
-                                        "coordinates":
-                                            [json.loads(open("polygon.json", "r").read())]
+                                        "coordinates": [json.loads(open("polygon.json", "r").read())]
                                     }
                                 }
                             }
                         }).count()
 
 open('correct_answer', 'w').write(str(result))
+#client.drop_database('sw_db')
