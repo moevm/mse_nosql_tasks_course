36d0b12f7e2f2199f9159f33edb456b112da272f
awethon <gorodgorod63@gmail.com>
2017-10-24 16:26:52 +0300

    np version

--- tasks/mongodb_near/near_creation.py
+++ tasks/mongodb_near/near_creation.py
@@ -6,7 +6,6 @@ from pymongo import MongoClient, GEOSPHERE
 mean = [55.75410148414108, 37.62049198150635]
 cov = [[0.04, 0],[0, 0.04]]
 xy = np.random.multivariate_normal(mean, cov, 5000)
-
 client = MongoClient()
 db = client.nr
 db.places.create_index([('loc', GEOSPHERE)])
