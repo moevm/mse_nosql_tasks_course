de7fc10300ec3d3659ebb05f87d6b347d6c2a836
MilanaGaiduk <milagaiduk@gmail.com>
2017-10-24 18:00:04 +0300

    added checking to another dir and moving correct_answer to /home/box/

--- tasks/mongodb_geointersects/generate_conditions.sh
+++ tasks/mongodb_geointersects/generate_conditions.sh
@@ -2,7 +2,11 @@
 
 key="esawwr3Xv41aSZCcVgz5AxcsBBBvtREssZEbFfbz3SCqzXBDSg"
 
+
+
+cd /home/box/repo/tasks/mongodb_geointersects/
 python3 geointersects_creation.py
+mv correct_answer /home/box/
 
 cd /home/box/
 echo ${key} | gpg --no-tty --passphrase-fd 0 -c /home/box/correct_answer
