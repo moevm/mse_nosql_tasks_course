83f19e5ac09df7b98194b3076660d561434bae7e
awethon <gorodgorod63@gmail.com>
2017-11-08 00:21:54 +0300

    remove ans sudo fix

--- tasks/mongodb_near/generate_conditions.sh
+++ tasks/mongodb_near/generate_conditions.sh
@@ -16,4 +16,4 @@ cd /home/box/
 
 echo ${key} | gpg --no-tty --passphrase-fd 0 -c /home/box/correct_answer
 chown box:box /home/box/correct_answer.gpg
-rm -f /home/box/correct_answer
+sudo rm -f /home/box/correct_answer
