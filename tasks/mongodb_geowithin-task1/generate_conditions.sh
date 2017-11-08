e7e0822f5022a1e6453f076ca99842327f6cd767
awethon <gorodgorod63@gmail.com>
2017-11-08 03:00:35 +0300

    repo rm polygon.js/json move clarification

--- tasks/mongodb_geowithin-task1/generate_conditions.sh
+++ tasks/mongodb_geowithin-task1/generate_conditions.sh
@@ -7,8 +7,10 @@ python3 geowithin_creation.py
 python3 geowithin_solution.py
 rm -f geowithin_solution.py
 mv correct_answer /home/box/
-
+mv polygon.js /home/box/
+mv polygon.json /home/box/
 cd /home/box/
+rm -rf repo
 
 echo ${key} | gpg --no-tty --passphrase-fd 0 -c /home/box/correct_answer
 chown box:box /home/box/correct_answer.gpg
