95e573f7f1b0616f06d534936a2fd36bdf4bc0cb
awethon <gorodgorod63@gmail.com>
2017-11-08 11:43:11 +0300

    repo rm test_polygon.js/json move

--- tasks/mongodb_geointersects/generate_conditions.sh
+++ tasks/mongodb_geointersects/generate_conditions.sh
@@ -7,8 +7,12 @@ key="esawwr3Xv41aSZCcVgz5AxcsBBBvtREssZEbFfbz3SCqzXBDSg"
 cd /home/box/repo/tasks/mongodb_geointersects/
 python3 geointersects_creation.py
 mv correct_answer /home/box/
+mv test_polygon.js /home/box/
+mv test_polygon.json /home/box/
 
 cd /home/box/
+rm -rf repo
+
 echo ${key} | gpg --no-tty --passphrase-fd 0 -c /home/box/correct_answer
 chown box:box /home/box/correct_answer.gpg
-rm /home/box/correct_answer
+sudo rm -f /home/box/correct_answer
