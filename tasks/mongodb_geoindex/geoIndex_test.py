66543ab87d30153341b4e090716944699774c3a3
MilanaGaiduk <milagaiduk@gmail.com>
2017-11-14 17:57:10 +0300

    added testing with assert

--- tasks/mongodb_geoindex/geoIndex_test.py
+++ tasks/mongodb_geoindex/geoIndex_test.py
@@ -4,14 +4,19 @@
 
 from pymongo import MongoClient, GEOSPHERE
 
-client = MongoClient()
-db = client.my_db
+def test_index(s):  #what is s?
+	client = MongoClient()
+	db = client.my_db
 
-#get information about indexes in db
-a = db.places.index_information()
+	#get information about indexes in db
+	a = db.places.index_information()
+	str_a = str(a.get("location_2dsphere"))
+	assert str_a != "None", "Ваш ответ не совпадает с правильным"
+
+test_index(1)
 
 
-if (str(a.get("location_2dsphere")) != "None"):   #i.e. index has been created
-	print("correct")
-else:
-	print("incorrect")
+#if (str(a.get("location_2dsphere")) != "None"):   #i.e. index has been created
+#	print("correct")
+#else:
+#	print("incorrect")
