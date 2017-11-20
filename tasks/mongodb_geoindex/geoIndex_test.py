22c3100a1b8d4ed9a8762f7ad922fb773d725e0c
MilanaGaiduk <milagaiduk@gmail.com>
2017-11-20 15:57:59 +0300

    turn to mongodb from shell

--- tasks/mongodb_geoindex/geoIndex_test.py
+++ tasks/mongodb_geoindex/geoIndex_test.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-
-from pymongo import MongoClient, GEOSPHERE
-
 def test_index(s):  #what is s?
-	client = MongoClient()
-	db = client.my_db
-
-	#get information about indexes in db
-	a = db.places.index_information()
-	str_a = str(a.get("location_2dsphere"))
-	assert str_a != "None", "Ваш ответ не совпадает с правильным"
-
-#if (str(a.get("location_2dsphere")) != "None"):   #i.e. index has been created
-#	print("correct")
-#else:
-#	print("incorrect")
+    result = s.run('mongo my_db --eval \'db.places.getIndexes()\' 2>/dev/null | grep -q loc_2dsphere')
+    assert result.succeeded, "Ваш ответ не совпадает с правильным"
