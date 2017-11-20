24d402203b289d1fc261ff6a9a59b18c41aebb7d
MilanaGaiduk <milagaiduk@gmail.com>
2017-11-20 21:37:54 +0300

    changed loc to location

--- tasks/mongodb_geoindex/geoIndex_test.py
+++ tasks/mongodb_geoindex/geoIndex_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-def test_index(s):  #what is s?
-    result = s.run('mongo my_db --eval \'db.places.getIndexes()\' 2>/dev/null | grep -q loc_2dsphere')
+def test_index(s):  
+    result = s.run('mongo my_db --eval \'db.places.getIndexes()\' 2>/dev/null | grep -q location_2dsphere')
     assert result.succeeded, "Ваш ответ не совпадает с правильным"
